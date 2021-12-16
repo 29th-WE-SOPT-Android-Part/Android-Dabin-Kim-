acbe58f8ebed969b0a5f2eb88004fd03fd66f0e2
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2021-12-16 20:38:38 +0900

    [ADD] Level2-Toolbar, NavigationComponent 연동

--- app/src/main/java/com/example/sopt_assignment_dabin/ui/OnBoardingActivity.kt
+++ app/src/main/java/com/example/sopt_assignment_dabin/ui/OnBoardingActivity.kt
@@ -4,11 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.sopt_assignment_dabin.R
 import com.example.sopt_assignment_dabin.databinding.ActivityOnBoardingBinding
 
 class OnBoardingActivity : AppCompatActivity() {
     private lateinit var binding: ActivityOnBoardingBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOnBoardingBinding.inflate(layoutInflater)
@@ -20,5 +27,16 @@ class OnBoardingActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
+        //Toolbar연결
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fc_container) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
